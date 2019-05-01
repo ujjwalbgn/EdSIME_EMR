4ad90b145674a2d82b7fe1c50138f57d24976f57
Ujjwal Bajagain <ubajagain@gmail.com>
2019-05-01 10:41:52 -0500

    active menu fixed

--- resources/views/patient/nav.blade.php
+++ resources/views/patient/nav.blade.php
@@ -1,5 +1,5 @@
 <ul class="nav nav-pills">
-    <li class="nav-item"><a class="nav-link" href="/admissionRecord/{{$admissionRecord->id}}/edit">Admission Record</a></li>
+    <li class="nav-item"><a class="nav-link {{ Request::segment(1) == 'admissionRecord' ? 'active' : '' }}" href="/admissionRecord/{{$admissionRecord->id}}/edit">Admission Record</a></li>
     <li class="nav-item"><a class="nav-link" href="" data-toggle="tab">Patient's History</a></li>
     <li class="nav-item"><a class="nav-link" href="" data-toggle="tab">Patient's Lab Records</a></li>
     <li class="nav-item"><a class="nav-link" href="" data-toggle="tab">Settings</a></li>
