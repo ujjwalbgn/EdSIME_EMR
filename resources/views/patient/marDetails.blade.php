dca6dfdd2a952a59f95bc7c8e59fa469385956cf
Ujjwal Bajagain <ubajagain@gmail.com>
2019-05-28 10:18:53 -0500

    minor updates

---
+++ resources/views/patient/marDetails.blade.php
@@ -0,0 +1,12 @@
+<div class="col-12">
+    <div class="card">
+        <div class="card-header">
+            <h6 class="card-title">Pateints Name<strong>{{$patient->name}}</strong></h6>
+
+        </div>
+        <div class="card-body">
+
+        </div>
+    </div>
+</div>
+
