1dbcfb4d611039a69f6b74c8eb24266364f7c5e8
Ujjwal Bajagain <ubajagain@gmail.com>
2019-06-10 11:31:50 -0500

    https middleware env changed

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -15,7 +15,7 @@ class HttpsProtocol
      */
     public function handle($request, Closure $next)
     {
-        if (!$request->secure() && App::environment() === 'production') {
+        if (!$request->secure() && env(APP_ENV) === 'production') {
             return redirect()->secure($request->getRequestUri());
         }
         return $next($request);
